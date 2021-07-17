4159f50b7d1f816660432b9ba0433330c2cf3d5b
Jeffrey Li <jffr.li1@gmail.com>
2021-07-17 14:13:14 -0700

    Implemented unoptimized pivoting.

--- main.c
+++ main.c
@@ -9,12 +9,12 @@ void printinverse(short int order, float m[order][order]) {
     /* Inputs */
     /* 1. Reading order of matrix */
     /* Augmenting Identity Matrix of Order n */
-    for (i = 0; i < 2*order; i++) { //zeroing the entire array
+    for (i = 0; i < 2*order; ++i) { //zeroing the entire array
         memset(a[i],0,2*order);
     }
-    for (i = 1; i <= order; i++)
+    for (i = 1; i <= order; ++i)
     {
-        for (j = 1; j <= order; j++)
+        for (j = 1; j <= order; ++j)
         {
             a[i][j] = m[i-1][j-1];
             if (i == j)
@@ -24,15 +24,15 @@ void printinverse(short int order, float m[order][order]) {
         }
     }
     /* Applying Gauss Jordan Elimination */
-    for (i = 1; i <= order; i++)
+    for (i = 1; i <= order; ++i)
     {
+        /*
         if (a[i][i] == 0.0)
         {
             // Instead of an error, we need to do a pivot here
             //printf("Implement Pivot");
             //exit(0);
             
-            //swap with next row open
             for (k = i+1; k <= order; ++k) {
                 if (a[k][i] != 0) {
                     for (int x = 0; x <= 2*order; ++x) {
@@ -43,13 +43,31 @@ void printinverse(short int order, float m[order][order]) {
                     break;
                 }
             }
+        }*/
+
+        //Pivoting
+        //swap with row with largest element
+        short int largest = a[i][i];
+        int n = i;
+        for (k = i+1; k <= order; ++k) { //find largest element
+            if (a[k][i] > largest) {
+                largest = a[k][i];
+                n = k;
+            }
         }
+        for (k = 0; k <= 2*order; ++k) { //swap rows
+            float t = a[i][k];
+            a[i][k] = a[n][k];
+            a[n][k] = t;
+        }
+
+
         for (j = 1; j <= order; j++)
         {
             if (i != j)
             {
                 ratio = a[j][i] / a[i][i];
-                for (k = 1; k <= 2 * order; k++)
+                for (k = 1; k <= 2 * order; ++k)
                 {
                     a[j][k] = a[j][k] - ratio * a[i][k];
                 }
@@ -57,18 +75,18 @@ void printinverse(short int order, float m[order][order]) {
         }
     }
     /* Row Operation to Make Principal Diagonal to 1 */
-    for (i = 1; i <= order; i++)
+    for (i = 1; i <= order; ++i)
     {
-        for (j = order + 1; j <= 2 * order; j++)
+        for (j = order + 1; j <= 2 * order; ++j)
         {
             a[i][j] = a[i][j] / a[i][i];
         }
     }
     /* Displaying Inverse Matrix */
     printf("\nInverse Matrix is:\n");
-    for (i = 1; i <= order; i++)
+    for (i = 1; i <= order; ++i)
     {
-        for (j = order + 1; j <= 2 * order; j++)
+        for (j = order + 1; j <= 2 * order; ++j)
         {
             printf("%f\t", a[i][j]);
         }
